f3f090dff182afcd399c90618b037d310f8b4a5f
Adenauer Yamin <adenauer@gmail.com>
2021-08-28 15:40:30 -0300

    Create le-anuncia-mqtt.c

---
+++ le-anuncia-mqtt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (void) {
+   char linha[100];
+   int compara;
+
+   while (1){
+   	scanf ("%[^\n]", linha);
+   	setbuf(stdin, NULL);
+   	compara = strcmp (linha, "????");
+
+   	if (compara == 0){
+       		system("mosquitto_pub -h broker.emqx.io -t temp-centro-pelotas -m 'adenauer'");
+	}
+	else {
+       		printf ("PRGM C: %s\n", linha);
+     	}
+   }
+   return EXIT_SUCCESS;
+}
