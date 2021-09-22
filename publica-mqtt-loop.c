b69777b77e96009d278caa1d18c7390fe4eb8c12
Adenauer Yamin <adenauer@gmail.com>
2021-09-21 21:26:17 -0300

    Update publica-mqtt-loop.c

--- publica-mqtt-loop.c
+++ publica-mqtt-loop.c
@@ -13,7 +13,7 @@ struct tm *data_hora_atual;
 	time_t segundos;
 	time(&segundos);
 	data_hora_atual = localtime(&segundos);
-	sprintf(comando, "mosquitto_pub -h broker.emqx.io -t temp-centro-pelotas -m 'Data e Hora Computador Adenauer: %d:%d:%d'", data_hora_atual->tm_hour, data_hora_atual->tm_min, data_hora_atual->tm_sec);
+	sprintf(comando, "mosquitto_pub -h broker.emqx.io -t pi2a/datahora -m 'Data e Hora Computador Adenauer: %d:%d:%d'", data_hora_atual->tm_hour, data_hora_atual->tm_min, data_hora_atual->tm_sec);
 	system(comando);
 	sleep(10);
 	}
