72d6ded6513e4c8a504753c2f2babe15b20eda9a
Adenauer Yamin <adenauer@gmail.com>
2021-09-28 15:13:25 -0300

    Update publica-frase-mqtt.c

--- publica-frase-mqtt.c
+++ publica-frase-mqtt.c
@@ -5,16 +5,43 @@
 int main( int argc, char *argv[ ] )
 {
 	int cont;
+	int i;
+	int j;
         char linha[256];
-	char comando[256];
+	char t[100];
+	char esp[] = " ";
+	char comando[256] = "mosquitto_pub -h broker.emqx.io -t pi2a/msgs -m '";
+	char finalcomando[] = "'";
 
-	for(cont=1; cont < argc; cont++){
-                strcat(linha,argv[cont]);
-		strcat(linha," ");
+        // Junta os argumentos separando por um espaco        
+        for(cont=1; cont < argc; cont++){
+
+	    int i = 0, j = 0;
+            while (linha[i] != '\0') {
+                   i++;
+            }
+            while ((linha[i++] = argv[cont][j++]) != '\0');
+
+            i = 0, j = 0;
+            while (linha[i] != '\0') {
+                   i++;
+            }
+            while ((linha[i++] = esp[j++]) != '\0');
+
 	}
-	strcat(comando, "mosquitto_pub -h broker.emqx.io -t pi2a/msgs -m '");
-	strcat(comando, linha);
-	strcat(comando, "'");
+        // Junta o comando MQTT com os argumentos
+        i = 0, j = 0;
+        while (comando[i] != '\0') {
+                i++;
+        }
+        while ((comando[i++] = linha[j++]) != '\0');
+
+        i = 0, j = 0;
+        while (comando[i] != '\0') {
+                 i++;
+        }
+        while ((comando[i++] = finalcomando[j++]) != '\0');
+
         system(comando);
 	return 0;
 }
